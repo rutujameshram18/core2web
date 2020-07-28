2fdee8c6a40be245e028288570780d2ef03b75ff
Rutuja Meshram <68662565+rutujameshram18@users.noreply.github.com>
2020-07-28 11:37:33 +0530

    Delete int.java

--- java/int.java
+++
@@ -1,9 +0,0 @@
-class IntDatatype {
-public static void main (String[] args) {
-int num = 14;
-System.out.println("num = " + num);
-System.out.println("Integer size = " + Integer.SIZE);
-System.out.println("Integer byte sixe = " + Integer.MAX_VALUE);
-System.out.println("Integer byte size = " + Integer.MIN_VALUE);
-}
-}
